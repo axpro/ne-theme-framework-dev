9d664a099895738dca511811b1d83f13e4434f94
Yannick Huard <huard.yan@gmail.com>
2016-01-28 17:25:11 +0100

    Updates

--- gitbook/plugins/gitbook-plugin-styleguide/script.js
+++ gitbook/plugins/gitbook-plugin-styleguide/script.js
@@ -1,32 +1,36 @@
 require(["gitbook"], function(gitbook) {
+  function onPageLoad() {
+    // Close dropdown menu on click
+    $('.styleguide .dropdown-menu a').on('click', function(event) {
+      $(this).parents('.dropdown').toggleClass('open');
+    });
 
-    function onPageLoad() {
-        // Close dropdown menu on click
-        $('.styleguide .dropdown-menu a').on('click', function (event) {
-            $(this).parents('.dropdown').toggleClass('open');
-        });
-
-        // Handle iframes
-        var iframes = $('iframe');
-        iframes.attr('src', function() {
-            var src = $(this).attr('data-src');
-            $(this).removeAttr('data-src');
-            return src;
-        });
-        iframes.on('load', function() {
-            $(this).removeClass('lazy');
-            $(this).iFrameResize({
-              heightCalculationMethod: 'max',
-              autoResize: false
-            });
-        });
-    }
-
-    if (history.pushState) {
-      gitbook.events.bind("page.change", function() {
-          onPageLoad();
+    // Handle iframes
+    var iframes = $('iframe');
+    iframes.attr('src', function() {
+      var src = $(this).attr('data-src');
+      $(this).removeAttr('data-src');
+      return src;
+    });
+    iframes.on('load', function() {
+      $(this).removeClass('lazy');
+      $(this).iFrameResize({
+        heightCalculationMethod: 'max',
+        autoResize: false
       });
-  } else {
-      onPageLoad();
+    });
   }
+
+  // "page.change" is triggered on first load, but due to a Chrome bug we have to check it
+  var loadedOnce = false;
+  gitbook.events.bind("page.change", function() {
+    if (loadedOnce) {
+      onPageLoad();
+    } else {
+      loadedOnce = true;
+    }
+  });
+
+  // Make sure to run this function at least once
+  onPageLoad();
 });
