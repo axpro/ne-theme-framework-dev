715130e7bc2ae79d1a4d3f42baed389ca82a0140
Yannick Huard <huard.yan@gmail.com>
2016-01-28 17:07:31 +0100

    Updates

--- 0.2.1/gitbook/plugins/gitbook-plugin-styleguide/script.js
+++ 0.2.1/gitbook/plugins/gitbook-plugin-styleguide/script.js
@@ -1,5 +1,6 @@
 require(["gitbook"], function(gitbook) {
-    gitbook.events.bind("page.change", function() {
+
+    function onPageLoad() {
         // Close dropdown menu on click
         $('.styleguide .dropdown-menu a').on('click', function (event) {
             $(this).parents('.dropdown').toggleClass('open');
@@ -19,5 +20,13 @@ require(["gitbook"], function(gitbook) {
               autoResize: false
             });
         });
-    });
+    }
+
+    if (history.pushState) {
+      gitbook.events.bind("page.change", function() {
+          onPageLoad();
+      });
+  } else {
+      onPageLoad();
+  }
 });
