aef7d333443c9f85e7c88c018ff194dbad3a37a3
Yannick Huard <huard.yan@gmail.com>
2016-02-02 16:38:23 +0100

    Updates

---
+++ components/file/scripts/file.js
@@ -0,0 +1,9 @@
+(function () { 'use strict';
+
+/**
+ * @file
+ * File component related behaviors.
+ */(function($){Drupal.behaviors.dt_file={attach:function(context){"use strict";var $button=$('.file__translations-control'),$translations=$('.file__translations-list');$button.click(function(){var self=this;$translations.collapse('toggle');$(self).toggleClass('is-collapsed');});}};})(jQuery);
+
+})();
+//# sourceMappingURL=file.js.map
